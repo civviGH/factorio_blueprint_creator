0fb835d81c2a50501073c8901de4e0187d12486b
civvi <hobby@chrvoss.de>
2018-11-14 01:56:36 +0100

    uniformen farbraum benutzt.

--- create_string.py
+++ create_string.py
@@ -3,40 +3,92 @@ import pickle
 from PIL import Image
 import sys
 from Tkinter import Tk
+import math
+from colormath.color_objects import sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
+import os.path
 
-# import shortest range dict
-print("Loading dictionary with shortest color ranges...")
-with open("color_coding_ranges.txt","rb") as f:
-    shortest_range = pickle.load(f)
-print("done")
+def getConfig(filename):
+    colors = {}
+    with open("configs/" + filename + ".dict", "r") as f:
+        for line in f:
+            foo = line.split(":")
+            p0 = foo[0].strip()
+            p1 = eval(foo[1].strip())
+            p2 = foo[2].strip()
+            toAdd = {}
+            toAdd["name"] = p0
+            toAdd["type"] = p2
+            colors[p1] = toAdd
+    return colors
 
-# resize image to wanted size
-print("Loading image and resizing it...")
-wanted_size = 50
-img = Image.open("img/pik.jpg")
-wpercent = (wanted_size/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img_sized = img.resize((wanted_size,hsize), Image.ANTIALIAS)
+def resizeImage(filename, wanted_size = 50):
+    filepath = "img/" + filename
+    if (os.path.isfile(filepath + ".jpg")):
+        filepath = filepath + ".jpg"
+    else:
+        filepath = filepath + ".png"
+    img = Image.open(filepath)
+    wpercent = (wanted_size/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img_sized = img.resize((wanted_size,hsize), Image.ANTIALIAS)
+    return(img_sized)
+
+def colorDistance(c1, c2):
+    #distance = math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2 + (c1[2]-c2[2])**2)
+    # normalize
+    co1 = list(c1)
+    co2 = list(c2)
+    for i in range(3):
+        co1[i] = co1[i] / 255.0
+        co2[i] = co2[i] / 255.0
+    c1_rgb = sRGBColor(co1[0], co1[1], co1[2])
+    c2_rgb = sRGBColor(co2[0], co2[1], co2[2])
+    c1_lab = convert_color(c1_rgb, LabColor)
+    c2_lab = convert_color(c2_rgb, LabColor)
+    delta_e = delta_e_cie2000(c1_lab, c2_lab)
+    return delta_e
+
+config_name = sys.argv[1]
+image_name = sys.argv[2]
+if len(sys.argv) > 3:
+    image_size = int(sys.argv[3])
+else:
+    image_size = 50
+
+colors = getConfig(config_name)
+img_sized = resizeImage(image_name, image_size)
+img_width = img_sized.size[0]
 img_pixels = img_sized.load()
-print("done")
-
-# iterate over pixels from image, add entity of right color
 
 # get background color from upper leftmost tile
-bg_color = img_pixels[0, 0]
+bg_color = img_pixels[0, 0][0:3]
+# emtpy dict with colors we can match
+matched_colors = {}
+
 print("Creating blueprint from image data...")
 bp = Blueprint()
-image_width = img_sized.size[0]
-for x in range(img_sized.size[0]):
-    #print("{}%".format((float(x+1)/image_width) * 100.0))
+for x in range(img_width):
+    print("{}%".format((float(x+1)/img_width) * 100.0))
     for y in range(img_sized.size[1]):
-        pixel_color = img_pixels[x, y]
+        pixel_color = img_pixels[x, y][0:3]
         if pixel_color == bg_color:
             continue
-        #if len(pixel_color) == 4:
-        #    if pixel_color[3] == 255:
-        #        continue
-        rtn_code = bp.addEntity(shortest_range[pixel_color[0:3]], (x,y), "tile")
+        if pixel_color in matched_colors:
+            rtn_code = bp.addEntity(matched_colors[pixel_color]["name"], (x,y), matched_colors[pixel_color]["type"])
+        else:
+            minimal_distance = 9999999
+            nearest_color = ""
+            for key,value in colors.iteritems():
+                distance = colorDistance(key, pixel_color)
+                if distance < minimal_distance:
+                    minimal_distance = distance
+                    nearest_color = key
+            matched_colors[nearest_color] = {}
+            matched_colors[nearest_color]["name"] = colors[nearest_color]["name"]
+            matched_colors[nearest_color]["type"] = colors[nearest_color]["type"]
+            rtn_code = bp.addEntity(colors[nearest_color]["name"], (x,y), colors[nearest_color]["type"])
         if rtn_code != 0:
             print(rtn_code)
 print("done")
